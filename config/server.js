4d07a72ef057fcb2d8e87a4344a68d8e6cc83a9a
devanshi <devanshi.katyal09@gmail.com>
2021-06-29 12:06:41 +0530

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd7bdefe9150be10734bd0b88d982457c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd97fee01-faba-4507-bab5-ad3a84ab1c5b'
+};
